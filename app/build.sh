14d9ff846bfc854c57ed86d08addfe79dbc4cf98
Katonic-ML-Marketplace <98009207+Katonic-ML-Marketplace@users.noreply.github.com>
2022-02-17 19:49:20 +0530

    Added App

--- app/build.sh
+++
@@ -1,3 +0,0 @@
-docker build -t katonic/apps:fraudulent-claim-detection .
-docker push katonic/apps:fraudulent-claim-detection
-# docker run --rm -p 8050:8050 katonic/apps:fraudulent-claim-detection
